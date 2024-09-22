a9b04f46b31a8462ccd28d09a00db2d595f70eb2
ste-vi <trozzato@gmail.com>
2024-09-22 11:27:45 +0300

    fixed small ui bugs

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,22 +1,20 @@
-import {Component} from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
-import {Transaction, TransactionType} from '../../models/transaction';
-import {TransactionService} from '../../services/api/transaction-service';
-import {DatePipe, DecimalPipe, NgForOf, NgIf} from "@angular/common";
-import {TransactionComponent} from "../common/transaction/transaction.component";
-import {SearchTransactionsService} from "../../services/communication/search-transactions-service";
-import {LoaderComponent} from "../common/loader/loader.component";
-import {
-  CreateTransactionButtonComponent
-} from "../common/transaction/create-transaction-button/create-transaction-button.component";
-import {TopExpensesWidgetComponent} from "../common/widgets/top-expenses-widget/top-expenses-widget.component";
-import {NavComponent} from "../common/nav/nav.component";
-import {HeaderComponent} from "../common/header/header.component";
-import {CategoryExpensesComponent} from "../common/categories/category-expenses/category-expenses.component";
-import {CategoriesComponent} from "../common/categories/categories.component";
-import {CreateTransactionComponent} from "../common/transaction/create-transaction/create-transaction.component";
-import {SearchTransactionsComponent} from "../common/transaction/search-transactions/search-transactions.component";
-import {TransactionViewComponent} from "../common/transaction/transaction-view/transaction-view.component";
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { Transaction } from '../../models/transaction';
+import { TransactionService } from '../../services/api/transaction-service';
+import { DatePipe, DecimalPipe, NgForOf, NgIf } from '@angular/common';
+import { TransactionComponent } from '../common/transaction/transaction.component';
+import { SearchTransactionsService } from '../../services/communication/search-transactions-service';
+import { LoaderComponent } from '../common/loader/loader.component';
+import { CreateTransactionButtonComponent } from '../common/transaction/create-transaction-button/create-transaction-button.component';
+import { TopExpensesWidgetComponent } from '../common/widgets/top-expenses-widget/top-expenses-widget.component';
+import { NavComponent } from '../common/nav/nav.component';
+import { HeaderComponent } from '../common/header/header.component';
+import { CategoryExpensesComponent } from '../common/categories/category-expenses/category-expenses.component';
+import { CategoriesComponent } from '../common/categories/categories.component';
+import { CreateTransactionComponent } from '../common/transaction/create-transaction/create-transaction.component';
+import { SearchTransactionsComponent } from '../common/transaction/search-transactions/search-transactions.component';
+import { TransactionViewComponent } from '../common/transaction/transaction-view/transaction-view.component';
 
 @Component({
   selector: 'app-dashboard',
